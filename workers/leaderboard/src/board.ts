a7bd127506f81913924ba4dea473f030e5c1318c
schloing <user.datagram.protocol@proton.me>
2025-01-06 18:14:14 +1100

    please work

--- workers/leaderboard/src/board.ts
+++
@@ -1,9 +0,0 @@
-import { DurableObject } from "cloudflare:workers";
-import { SortedMap } from './SortedMap';
-// SortedMap and SortedArray from https://github.com/bacali95/sweet-collections
-
-export class LeaderboardInstance {
-    async fetch(request: Request, env: Env) {
-        return new Response("Hello World");
-    }
-}
--- workers/leaderboard/src/index.ts
+++ workers/leaderboard/src/index.ts
@@ -1,25 +1,46 @@
-import { randomUUID, UUID } from 'node:crypto';
-import * as table from '../../../src/lib/server/db/schema';
-import { eq } from 'drizzle-orm';
+import { DurableObject } from "cloudflare:workers";
+import { BoardInstruction } from "../../../src/lib/board"
+import { type UUID } from 'node:crypto';
+
+export class Connections extends DurableObject {
+	sessions: Map<WebSocket, BoardInstruction[]>;
+	state: DurableObjectState;
+
+	constructor(state: DurableObjectState, env: Env) {
+		super(state, env);
+		this.sessions = new Map();
+		this.state = state;
+
+		state.getWebSockets().forEach((ws) => {
+			const meta = ws.deserializeAttachment();
+			this.sessions.set(ws, { ...meta });
+		});
+	}
+
+	public async fetch(request: Request): Promise<Response> {
+		if (request.headers.get("Upgrade") != "websocket") return new Response(null, { status: 400 });
+		const pair = new WebSocketPair();
+		await this.handleSession(pair[1], <BoardInstruction><unknown>request.body);
+		return new Response(null, { status: 101, webSocket: pair[0] });
+	}
+
+	private async handleSession(ws: WebSocket, board: BoardInstruction) {
+		this.state.acceptWebSocket(ws);
+		ws.serializeAttachment({ ...ws.deserializeAttachment() ?? board });
+		const ins = this.sessions.get(ws) ?? [];
+		ins.push(board);
+		this.sessions.set(ws, ins);
+	}
+}
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext,
-	): Promise<Response> {
-		const authHeader = request.headers.get("authentication");
-		const boardHeader = request.headers.get("board");
+	async fetch(request: Request, env: Env): Promise<Response> {
+		if (request.method === "POST") {
+			const id = env.Connections.idFromName("GLOBAL_CONNECTIONS");
+			const stub = env.Connections.get(id);
+			return stub.request(request);
+		}
 
-		if (!authHeader || !boardHeader)
-			return Response.json({ code: 404, error: "header not found" });
-
-		const boardDatabase = env.DB.select().from(table.boards).where(eq(table.boards.id, boardHeader));
-
-		const board = env.LeaderboardInstance.idFromName(boardHeader);
-
-		return new Response("hello world");
+		return new Response(null, { status: 400 });
 	},
 } satisfies ExportedHandler<Env>;
-
-export * from "./board";
