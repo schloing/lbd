debd00c0cb052a756780ff924da8e5dd7d7ff7a8
schloing <user.datagram.protocol@proton.me>
2025-01-07 17:49:17 +1100

    begin worker implementation into board view

--- src/routes/board/[id]/+page.svelte
+++ src/routes/board/[id]/+page.svelte
@@ -3,8 +3,28 @@
 	import Rankings from '$/components/Rankings.svelte';
 	import type { PageServerData } from './$types';
 	import { page } from '$app/state';
+	import { type BoardInstruction, BoardOperation } from '$/lib/board';
+	import { onMount } from 'svelte';
+
 	const id: string = page.params.id;
 	let { data }: { data: PageServerData } = $props();
+	let ready = $state<boolean>(false);
+
+	onMount(async () => {
+		const websocket = new WebSocket(
+			import.meta.env.MODE == 'development'
+				? 'ws://localhost:8989'
+				: import.meta.env.VITE_WORKER_URL
+		);
+
+		websocket.addEventListener('message', (event) => {
+			const data = JSON.parse(event.data);
+			console.log(data);
+		});
+
+		websocket.onopen = () => websocket.send(JSON.stringify({ message: 'hello world' }));
+		websocket.onclose = () => websocket.close;
+	});
 </script>
 
 <section class="boardInfo">
