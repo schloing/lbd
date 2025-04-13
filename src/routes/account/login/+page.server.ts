ca96d239f71d77f313482896df241d8e87e3b06d
schloing <user.datagram.protocol@proton.me>
2025-04-13 22:25:32 +1000

    old logo

--- src/routes/account/login/+page.server.ts
+++ src/routes/account/login/+page.server.ts
@@ -1,5 +1,3 @@
 import { signIn, signOut } from '$/auth';
 import type { Actions } from './$types';
 export const actions: Actions = { default: signIn };
-import { noAccountRequired } from '$/lib/server/restricted';
-export const load = noAccountRequired;
