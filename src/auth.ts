6775f95cf49c29e0b4e45de7493bb645e903ac5d
schloing <user.datagram.protocol@proton.me>
2025-11-21 21:51:28 +1100

    snapshot before switching to better auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,10 @@
-import { SvelteKitAuth, type DefaultSession } from '@auth/sveltekit';
+import { SvelteKitAuth, type DefaultSession, type Profile, type User } from '@auth/sveltekit';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import Google from '@auth/sveltekit/providers/google';
 import Discord from '@auth/sveltekit/providers/discord';
 import { db } from '$/index';
+import type { OAuthConfig, OAuthUserConfig, ProfileCallback, Provider } from '@auth/core/providers';
+import type { TokenSet } from '@auth/core/types';
 
 declare module '@auth/sveltekit' {
 	interface Session {
@@ -10,20 +12,29 @@ declare module '@auth/sveltekit' {
 	}
 }
 
+const OAuthProfileCallback: ProfileCallback<Profile> = (profile: Profile, tokens: TokenSet) => {
+	return {
+		...profile,
+		role: profile.role ?? "new",
+	} as User;
+}
+
+type OAuthProvider<T extends Profile> = (options: OAuthUserConfig<T>) => OAuthConfig<T>;
+
+const providers: OAuthProvider<Profile>[] = [
+	Google as OAuthProvider<Profile>,
+	Discord as OAuthProvider<Profile>
+];
+
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	adapter: DrizzleAdapter(db),
-	providers: [Google, Discord],
+	providers: [
+		Google, Discord
+	],
 	callbacks: {
 		async session({ session, user }) {
-			const schemaUser = user;
-
 			return {
-				user: {
-					id: schemaUser.id,
-					name: schemaUser.name,
-					email: schemaUser.email,
-					image: schemaUser.image,
-				},
+				user: session.user,
 				expires: session.expires
 			};
 		}
