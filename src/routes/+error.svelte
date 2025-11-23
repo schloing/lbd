e1b870d2e764cba98e446f51c3957b46a2bafc51
schloing <user.datagram.protocol@proton.me>
2025-11-23 13:59:07 +1100

    top level error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<h1><span class="error">{page.status} - {page.error?.message}</span></h1>
+<p>come back in a few minutes</p>
+<p>problem still happening? <a href="https://github.com/schloing/lbd/issues/">report the bug</a></p>
+<p>know how to fix it? <a href="https://github.com/TheCalculus/lbd/">fix the bug yourself here</a></p>
