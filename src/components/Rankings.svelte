ab9d5e75421388bf0a3a8668e7244d7610f3f092
schloing <user.datagram.protocol@proton.me>
2025-12-10 23:54:36 +1100

    fix to use updated props in Rank

--- src/components/Rankings.svelte
+++ src/components/Rankings.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
-	import Rank from './Rank.svelte';
-	let { rankings }: { rankings: { username: string; score: string }[] } = $props();
+	import type { RankUser } from '$/lib/client/rankuser';
+	import Td from './Td.svelte';
+	let { rankings }: { rankings: RankUser[] } = $props();
+	// svelte-ignore non_reactive_update
 	let currentInternalRanking = 0;
 </script>
 
 <table>
+	<colgroup>
+		<col span="1" style="width: 15%;" />
+		<col span="1" style="width: 70%;" />
+		<col span="1" style="width: 15%;" />
+	</colgroup>
+
 	<tbody>
-		<Rank isHeader={true} />
+		<!-- TODO: add alternative -->
 		{#if rankings.length > 0}
-			{#each rankings as ranking}
-				<Rank
-					rank={++currentInternalRanking}
-					username={ranking.username}
-					points={parseInt(ranking.score)}
-				/>
+			{#each rankings as rankUser}
+				<tr class="rank">
+					<Td>{++currentInternalRanking}</Td>
+					<Td>
+						{rankUser.name}
+						{#if rankUser.accountAssociated}
+							<a href="/account/{rankUser.uuid}">@{rankUser.username}</a>
+						{:else}
+							<span class="stealth">(no account)</span>
+						{/if}
+					</Td>
+					<Td>{rankUser.score}</Td>
+				</tr>
 			{/each}
-		{:else}
-			<Rank isError={true} />
 		{/if}
 	</tbody>
 </table>
@@ -24,5 +37,27 @@
 <style scoped>
 	table {
 		width: 100%;
+		border: none;
+		border-collapse: separate;
+		border-spacing: 0 0.2em;
+		margin: 0;
+	}
+
+	.rank {
+		background: var(--bg-color);
+		height: 3em;
+		transition: background 150ms;
+	}
+
+	.rank:hover {
+		background: var(--sub-color);
+	}
+
+	.rank .stealth {
+		transition: 150ms color;
+	}
+
+	.rank:hover .stealth {
+		color: var(--sub-alt-color);
 	}
 </style>
