c634e70478a8833046fff2e49d0f5108de8ae9d0
schloing <user.datagram.protocol@proton.me>
2025-12-11 23:02:14 +1100

    early version docs route

---
+++ src/routes/docs/+page.svelte
@@ -0,0 +1,112 @@
+<section id="speed">
+	<h2>Speed</h2>
+
+	<p class="note">TODO</p>
+</section>
+
+<section id="price">
+	<h2>Price</h2>
+
+	<p class="note">
+		Leaderbored is free for individuals and organisations with less than 150 unique users over all
+		their boards by default.
+	</p>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Features</th>
+				<th>Free Tier</th>
+				<th>Mid Tier</th>
+				<th>High Tier</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Price per month</td>
+				<td>$0</td>
+				<td>$5</td>
+				<td>TBD</td>
+			</tr>
+			<tr>
+				<td>No maximum number of boards</td>
+				<td>✔️</td>
+				<td>✔️</td>
+				<td>✔️</td>
+			</tr>
+			<tr>
+				<td>No guaranteed minimum allocation of RAM and CPU</td>
+				<td>✔️</td>
+				<td>✔️</td>
+				<td>✔️</td>
+			</tr>
+			<tr>
+				<td>Real-time updates on the web app and API</td>
+				<td>✔️</td>
+				<td>✔️</td>
+				<td>✔️</td>
+			</tr>
+			<tr>
+				<td>No rate limiting for API requests</td>
+				<td>❌</td>
+				<td>✔️</td>
+				<td>✔️</td>
+			</tr>
+			<tr>
+				<td>No limit on the number of unique users</td>
+				<td>❌</td>
+				<td>✔️</td>
+				<td>✔️</td>
+			</tr>
+			<tr>
+				<td>Guaranteed allocation of system resources</td>
+				<td>❌</td>
+				<td>❌</td>
+				<td>✔️</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<p class="note">
+		The cost for the highest tier depends on the amount of system resources you request to allocate.
+	</p>
+</section>
+
+<section id="applications">
+	<h2>Applications</h2>
+
+    <ul>
+        <li>MMORPG Games</li>
+    </ul>
+</section>
+
+<style scoped>
+	table {
+		width: 100%;
+		border-collapse: collapse;
+		margin: 20px 0;
+	}
+
+	th,
+	td {
+		border: 1px solid var(--sub-color);
+		padding: 12px;
+		text-align: left;
+	}
+
+	th {
+		background-color: var(--bg-color);
+	}
+
+	section {
+		text-align: left;
+	}
+
+	.note {
+		padding: 1em;
+		margin: 0.5em 0;
+		line-height: 1em;
+		background: var(--sub-color);
+		border-radius: 0.3em;
+	}
+</style>
