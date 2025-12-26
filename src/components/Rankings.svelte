1d20e7f44f4a0cc9e696c0683969231c3faa6b8e
schloing <user.datagram.protocol@proton.me>
2025-12-26 23:48:53 +1100

    add buttons for rank actions, not yet functional

--- src/components/Rankings.svelte
+++ src/components/Rankings.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
 	import type { RankUser } from '$/lib/client/rankuser';
-	import {
-		DeleteIcon,
-		MinusIcon,
-		PencilIcon,
-		PlusIcon,
-		Trash2Icon,
-		TrashIcon
-	} from 'lucide-svelte';
+	import { MinusIcon, PencilIcon, PlusIcon, Trash2Icon } from 'lucide-svelte';
 	let { rankings, authorized }: { rankings: RankUser[]; authorized: boolean } = $props();
 	// svelte-ignore non_reactive_update
 	let currentInternalRanking = 0;
+	let showTrash = $state(false);
 </script>
 
 <table>
@@ -25,7 +19,17 @@
 		{#if rankings.length > 0}
 			{#each rankings as rankUser}
 				<tr class="rank">
-					<td>{++currentInternalRanking}</td>
+					<td
+						class="ranking"
+						onmouseenter={() => (showTrash = true)}
+						onmouseleave={() => (showTrash = false)}
+					>
+						<span class="rank-number">{++currentInternalRanking}</span>
+						<button class="danger">
+							<Trash2Icon />
+						</button>
+					</td>
+
 					<td>
 						{rankUser.name}
 						{#if rankUser.accountAssociated}
@@ -34,7 +38,28 @@
 							<span class="stealth">(no account)</span>
 						{/if}
 					</td>
+
 					<td>{rankUser.score}</td>
+
+					{#if authorized}
+						<td class="actions">
+							<div>
+								<!-- TODO: +/- 10 might not be meaningful, or too meaningful
+								 	 calculate a meaningful change of points -->
+								<!-- eg. 10 points is a lot on a leaderboard tracking wins in bedwarz 
+								 	 but not a lot for kills in skywarz -->
+								<button class="action">
+									<PlusIcon />
+									<span>10</span>
+								</button>
+
+								<button class="action">
+									<MinusIcon />
+									<span>10</span>
+								</button>
+							</div>
+						</td>
+					{/if}
 				</tr>
 			{/each}
 		{/if}
@@ -42,6 +67,41 @@
 </table>
 
 <style scoped>
+	.ranking {
+		position: relative;
+		width: 50px;
+		text-align: center;
+	}
+
+	.ranking .rank-number {
+		display: block;
+		transition: opacity 0.2s ease;
+	}
+
+	.ranking .danger {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -65%);
+		opacity: 0;
+		pointer-events: none;
+		transition: opacity 0.2s ease;
+		background: none;
+		border: 1px solid var(--danger-color);
+		border-radius: 0.5rem;
+		padding: 0.5em;
+		cursor: pointer;
+		color: var(--text-color);
+	}
+
+	.ranking:hover .danger {
+		opacity: 1;
+		pointer-events: auto;
+	}
+
+	.ranking:hover .rank-number {
+		opacity: 0;
+	}
 	table {
 		width: 100%;
 		border: none;
@@ -50,21 +110,61 @@
 		margin: 0;
 	}
 
+	.actions > div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		gap: 0.4em;
+		margin: 0 0.2em;
+	}
+
+	.editable {
+		cursor: pointer;
+	}
+
+	.rank:hover,
+	.active-rank {
+		background: var(--sub-color);
+	}
+
+	.rank:hover .stealth,
+	.active-rank .stealth {
+		color: var(--sub-alt-color);
+	}
+
+	.rank:hover .action,
+	.active-rank .action {
+		background: var(--sub-alt-color);
+	}
+
+	.action {
+		background: none;
+		border-radius: 0.5rem;
+		border: 1px solid var(--sub-color);
+		max-height: 50px;
+		transition:
+			background 150ms,
+			border-color 150ms,
+			color 150ms;
+		display: flex;
+		align-items: center;
+		gap: 0.2em;
+		font-size: 1.2rem;
+	}
+
+	.action:hover {
+		background: var(--text-color) !important;
+		color: var(--bg-color);
+	}
+
 	.rank {
 		background: var(--bg-color);
 		height: 3em;
 		transition: background 150ms;
 	}
 
-	.rank:hover {
-		background: var(--sub-color);
-	}
-
 	.rank .stealth {
 		transition: 150ms color;
-	}
-
-	.rank:hover .stealth {
-		color: var(--sub-alt-color);
 	}
 </style>
