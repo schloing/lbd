bf8cfb64c67a66e257bb4bd32502fae51930f3e5
schloing <user.datagram.protocol@proton.me>
2026-06-30 17:26:27 +1000

    fix: use prod url in production

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-export const db = drizzle(process.env.AUTH_DRIZZLE_URL!);
+export const db = drizzle(
+    process.env.NODE_ENV === 'production'
+        ? process.env.AUTH_DRIZZLE_URL_PROD || process.env.AUTH_DRIZZLE_URL!
+        : process.env.AUTH_DRIZZLE_URL!
+);
